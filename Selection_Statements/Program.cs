96a006cbf995fd47d51a80cb5e217b2ec2fbf773
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-07-07 12:29:35 -0400

    First commit

---
+++ Selection_Statements/Program.cs
@@ -0,0 +1,21 @@
+﻿
+var r = new Random();
+int favNumber = r.Next(1,1000);
+
+
+    Console.WriteLine("Guess a number.");
+    var userInput = int.Parse(Console.ReadLine());
+
+
+    if (userInput < favNumber)
+    {
+        Console.WriteLine("Too low.");
+    }
+    else if (userInput > favNumber)
+    {
+        Console.WriteLine("Too high.");
+    }
+    else
+    {
+        Console.WriteLine("Congratulations, you are correct!");
+    }
